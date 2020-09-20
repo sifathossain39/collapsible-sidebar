2fe1b6daf18f55888a9e155a395cc6a414233636
Khondakar Shefat Hossain <61610790+sifathossain39@users.noreply.github.com>
2020-09-20 11:57:07 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+
+    $(".sidetoggle").click(function () {
+        $(".navbar").toggleClass("active")
+        $(".main_container").toggleClass("active")
+    });
+
+})
